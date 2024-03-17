58ce347a3dfd7ef2f87d25a6b47b510c9ef8f26c
gongguan <1285462534@qq.com>
2024-03-17 14:44:42 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by gongguan on 2024/3/17.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
